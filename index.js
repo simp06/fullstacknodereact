02c3496cd486e999b95419aaf58fc8b7e8fef9f8
sebas <simpdev7@gmail.com>
2020-01-17 17:44:05 -0300

    avance

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
-
-app.get("/",function(req,res){
-res.send({hi:"there"});
-});
-
 const PORT = process.env.PORT || 5000;
+passport.use(new GoogleStrategy());
 app.listen(PORT);
+/*
+Client ID
+182130924998-qt12b9lrr79h1p9tgs0j2im8i6aqr389.apps.googleusercontent.com
+Client secret
+Gb_k5uG6zkKvSzdggQKuRx0n */
