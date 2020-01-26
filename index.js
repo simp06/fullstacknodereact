f6b96a10e9f7f1bff86f4e5858b1c7ca16a8445f
sebas <simpdev7@gmail.com>
2020-01-26 20:10:46 -0300

    connecto to mongoose

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
  require("./service/passport");
- 
+ require("./models/User");
+ const keys = require("./config/keys");
 const app = express();
 require("./routes/authRoutes")(app); 
 const PORT = process.env.PORT || 5000;
+mongoose.connect(keys.mongoURI);
 
 
 app.listen(PORT);
